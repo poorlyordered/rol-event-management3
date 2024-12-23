2a186136e32aeead6c072ec42c4427b96944e37b
poorlyordered <netgleb@gmail.com>
2024-12-23 17:21:48 -0600

    Implement role-based authentication system with documentation

---
+++ src/lib/types/enums.ts
@@ -0,0 +1,15 @@
+export enum StaffRole {
+    Owner = 'owner',
+    PlatformAdmin = 'platform_admin',
+    CustomerService = 'customer_service',
+    TournamentDirector = 'tournament_director',
+    TournamentCoordinator = 'tournament_coordinator',
+    LeagueDirector = 'league_director',
+    LeagueCoordinator = 'league_coordinator'
+}
+
+export enum OrgRole {
+    Owner = 'org_owner',
+    Manager = 'org_manager',
+    Staff = 'org_staff'
+}
